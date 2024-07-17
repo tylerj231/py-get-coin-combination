5641fdf16480cf44c990ce29f9f5a46730658a65
Mykola <jacktyler2391@gmail.com>
2024-07-17 19:19:35 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,20 @@
 from app.main import get_coin_combination
+import pytest
 
 
 # write your tests here
+@pytest.mark.parametrize(
+    "cents, result",
+    [
+        (1, [1, 0, 0, 0]),
+        (6, [1, 1, 0, 0]),
+        (17, [2, 1, 1, 0]),
+        (50, [0, 0, 0, 2]),
+        (0, [0, 0, 0, 0]),
+        (25, [0, 0, 0, 1]),
+        (11, [1, 0, 1, 0]),
+        (30, [0, 1, 0, 1])
+    ]
+)
+def test_return_correct_combination_of_coins(cents: int, result: list[int]) -> None:
+    assert get_coin_combination(cents) == result
